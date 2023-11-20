63a376bc4c6b77967c9c18fcd50e4a602b607cec
Loxik <hello@loxik.dev>
2023-11-20 15:49:50 +0100

    Update .gitignore, index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.MessageContent
+    GatewayIntentBits.MessageContent,
   ],
 });
 
